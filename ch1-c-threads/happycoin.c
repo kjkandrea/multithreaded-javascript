a4f54d121fcbd89c0d10442e12278e029c6aa988
kjkandrea <kjkandrea@gmail.com>
2022-10-11 21:33:37 +0900

    chore: num이 Happycoin 인지, 또 10,000 으로 나눠지는지 확인. 이를 부하를 위해 10000000 번 반복

--- ch1-c-threads/happycoin.c
+++ ch1-c-threads/happycoin.c
@@ -34,13 +34,20 @@ bool is_happy(uint64_t num) {
     return num == 1;
 }
 
+// num이 Happycoin 인지, 또 10,000 으로 나눠지는지 확인
+bool is_happycoin(uint64_t num) {
+    return is_happy(num) && num % 10000 == 0;
+}
+
 int main() {
     uint32_t seed = time(NULL);
     int count = 0;
-    for (int i = 1; i < 100;i++) {
-        uint64_t random_num = is_happy(random64(&seed));
-        printf("%" PRIu64 " ", random_num);
-        count++;
+    for (int i = 1; i < 10000000;i++) {
+        uint64_t random_num = random64(&seed);
+        if (is_happycoin(random_num)) {
+            printf("%" PRIu64 " ", random_num);
+            count++;
+        }
     }
     printf("\ncount %d\n", count);
     return 0;
