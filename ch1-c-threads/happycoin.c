2a123c8ddc61c1083aa0c3b99bd075918a3abcf7
kjkandrea <kjkandrea@gmail.com>
2022-10-11 21:26:58 +0900

    chore: ch1. 각 자릿수를 제곱하고, 그 값을 모두 더함

--- ch1-c-threads/happycoin.c
+++ ch1-c-threads/happycoin.c
@@ -15,11 +15,22 @@ uint64_t random64(uint32_t * seed) {
     return result;
 }
 
+// 각 자릿수를 제곱하고, 그 값을 모두 더함
+uint64_t sum_digits_squared(uint64_t num) {
+    uint64_t total = 0;
+    while(num > 0) {
+        uint64_t num_mod_base = num % 10;
+        total += num_mod_base * num_mod_base;
+        num = num / 10;
+    }
+    return total;
+}
+
 int main() {
     uint32_t seed = time(NULL);
     int count = 0;
     for (int i = 1; i < 10;i++) {
-        uint64_t random_num = random64(&seed);
+        uint64_t random_num = sum_digits_squared(random64(&seed));
         printf("%" PRIu64 " ", random_num);
         count++;
     }
